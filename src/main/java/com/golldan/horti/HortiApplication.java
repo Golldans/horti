22966ba44177d07a7dca9dd58045dc95fbe1b31c
cristo.ferreiro <cristiano.henriqueferreirabp@gmail.com>
2023-11-15 13:06:50 -0300

    feat: initial commit

---
+++ src/main/java/com/golldan/horti/HortiApplication.java
@@ -0,0 +1,13 @@
+package com.golldan.horti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HortiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/golldan/horti/HortiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golldan.horti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HortiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
